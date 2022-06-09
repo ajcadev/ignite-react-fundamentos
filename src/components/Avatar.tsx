a0568d86f6954fcdb060e5f62102f094ea21d91e
Cardoso <cardosoassuncao@gmail.com>
2022-06-09 08:49:50 -0400

    add extension of interface

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,14 +1,22 @@
+import { ImgHTMLAttributes } from 'react'
 import styles from './Avatar.module.scss'
 
-type AvatarProps = {
-  src: string
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   hasBorder?: boolean
 }
-export function Avatar({src, hasBorder=true}: AvatarProps) {
+export function Avatar({ hasBorder=true, ...props }: AvatarProps) {
   return (
     <img
       className={ hasBorder ? styles.avatarWithBorder : styles.avatar }
-      src={ src }
+      {...props}
     />
   )
 }
+
+// Observe que o retorno do componente Avatar em uma img
+// Aqui estamos fazendo desestruturação usando rest operator(...pros)
+// O que significa que todas as propriedades do elemento img estão
+// sendo capturadas no objeto props e depois sendo repassadas ao elemento img.
+// Isso nos dá a flexibilidade de no momento de invocar o componente Avatar
+// invocá-lo com uma, algumas ou todas as propriedades de um elemento img!!!
+// Observe que essa flexibilidade foi possível devido à interface AvatarProps
